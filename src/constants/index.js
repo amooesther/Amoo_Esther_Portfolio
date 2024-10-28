3712466b40ebe3f813b060d8eed7908b0328af03
Esther Toyin <amooesther2503@gmail.com>
2024-10-28 10:46:33 +0000

    project update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,6 @@
-import project1 from "../assets/projects/project-1.png";
-import project2 from "../assets/projects/project-2.png";
-import project3 from "../assets/projects/project-3.png";
-import project4 from "../assets/projects/project-4.jpg";
+import project1 from "../assets/projects/project1.jpg";
+import project2 from "../assets/projects/project2.png";
+
 
 export const HERO_CONTENT = ` I am a Frontend software developer building solutions that meet market needs and drive business growth + user satisfaction.
 Proficient in HTML, CSS & JavaScript. Experienced with React js. Well versed in agile methodologies and an excellent team player.`;
@@ -14,47 +13,40 @@ I thrive in collaborative environments where teamwork and communication are key 
 
 export const EXPERIENCES = [
   {
-    year: "2024 - Present",
+    year: "Oct. 2024 - present",
+    role: "Frontend Developer",
+    company: "Intern Pulse",
+    description: `Developed and maintained responsive web pages using React.js, collaborating with designers and backend developers to implement engaging and user-friendly interfaces. Integrated backend services and APIs to facilitate real-time, dynamic data displays, enhancing user experience. Leveraged Git for version control, utilizing branching, merging, and pull requests to streamline team collaboration. Additionally, contributed to a reusable component library to ensure design consistency and development efficiency across projects.`,
+    technologies: ["HTML", "CSS", "React Js", "JavaScript"]
+  },
+  {
+    year: "May 2024 - Oct. 2024",
     role: "Frontend Developer",
     company: "Afribreath ltd.",
     description: `Developed and maintained responsive web pages using React.js, collaborating with designers and back-end developers to implement user interfaces. Connected web pages to backend services and APIs to fetch and display dynamic data. Utilized Git for source code management, including branching, merging, and pull requests to facilitate team collaboration. Additionally, created and maintained a library of reusable components to ensure consistency and efficiency across the application.`,
     technologies: ["Javascript", "React.js", "Tailwind css", "HTML"],
-  },
-  {
-    year: "2023 - 2024",
-    role: "Product designer",
-    company: "Empower Her Community",
-    description: `Collaborated with the design team to develop wireframes, prototypes, and visual designs, conducting user research to identify user needs and preferences. Participated in design critiques and provided constructive feedback, creating and presenting design concepts to stakeholders. Additionally, assisted with the design of marketing materials and social media content.`,
-    technologies: ["HTML", "CSS", "Figma", ]
   }
   
 ];
 
 export const PROJECTS = [
   {
+    title: "Cypto Currency Tracker Dashboard",
+    image: project1,
+    description:
+      "A cryptocurrency dashboard application for tracking real-time crypto data, with features such as portfolio tracking, live price updates, and performance monitoring.",
+    technologies: ["HTML", "CSS", "JavaScript"],
+     link: "https://crypto-tracker-dashboard-esther-amoo.vercel.app/"
+  },
+  {
     title: "E-Commerce Website",
-  image: project1,
+  image: project2,
   description:
     "A Frozen food e-commerce website with features like product listing, shopping cart, and user authentication.",
   technologies: ["HTML", "CSS", "React", "Node.js", "Firebase"],
   link: "https://kaydas-amooesther2503gmailcoms-projects.vercel.app/"
-},
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-     link: "https://glowing-frangollo-c6e350.netlify.app/"
-  },
-  {
-    title: "A Quiz App",
-    image: project3,
-    description:
-      "A fun quiz app to test your knowledge, challenge friends, and track your progress across tech topics.",
-    technologies: ["HTML", "CSS", "JavaScript"],
-    link: "https://splendorous-lebkuchen-43bb0e.netlify.app/"
-  }
+}
+
 ];
 
 export const CONTACT = {
